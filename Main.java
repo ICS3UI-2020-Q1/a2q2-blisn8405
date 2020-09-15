3344641fe5937c2fce1a6d7e0e13d2a3f7587fdb
blisn8405 <blisn8405@wrdsb.ca>
2020-09-15 14:54:50 +0000

    finished question 2

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+     // create a scanner for user input
+Scanner input = new Scanner(System.in);
     
+    //define number a
+    final int A = 5;
+
+    //define number b
+   final int B = 3;
+
+  if(A>= B){
+  //say if the number 5 is bigger
+  System.out.println("The biggest number is 5");
+  } else {
+// say if the number 3 is bigger
+System.out.println("The biggest number is 3");
+  }
   }
 }
